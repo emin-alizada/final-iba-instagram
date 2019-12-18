6f5e04c5ba0851082045d2b5403f1793c64b8e82
Cabbar Hasanov <cabbarhesenov1999@gmail.com>
2019-12-18 20:50:30 +0400

    post-comment ready

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.scss';
 import Icon from "./components/sharedComponents/Icon";
 import SignInPage from "./components/SignInPage";
 import MainFeed from "./components/MainFeed";
+import PostComment from "./components/sharedComponents/postComment";
 
 function App() {
 
   return (
     <div className="App">
         {/*<SignInPage/>*/}
-        <MainFeed/>
+        {/*<MainFeed/>*/}
+        <PostComment/>
 
     </div>
   );
--- src/components/sharedComponents/postComment/index.js
+++ src/components/sharedComponents/postComment/index.js
@@ -11,39 +11,45 @@ class PostComment extends React.Component {
                 <div className={"post-comment-window"}>
                     <img src={require("./img/post-comment-dog.png")} alt=""/>
                     <div className={"comment-section"}>
-                        <div className="comment-section-top">
-                            <div className={"comment-section-user"}>
-                                <img src={require("./img/laura-comment.png")} alt="" className={"comment-section-user-img"}/>
-                                <p className={"comment-section-username"}>laura.stanley</p>
-                            </div>
-                            <Icon name = {"options"}/>
-                        </div>
+ <div className="comments-and-user">
+     <div className="comment-section-top">
+         <div className={"comment-section-user"}>
+             <img src={require("./img/laura-comment.png")} alt="" className={"comment-section-user-img"}/>
+             <p className={"comment-section-username"}>laura.stanley</p>
+         </div>
+         <Icon name = {"options"}/>
+     </div>
 
-                        <div className={"posted-comments"}>
-                            <PostedComment/>
-                            <PostedComment/>
-                            {/*<div className={"comment"}>*/}
-                            {/*    <img src={require("img/maria-comment.png")} alt="" className={"commenter-img"}/>*/}
-                            {/*    <div className={"comment-details"}>*/}
-                            {/*        <div className={"commenter-name-and-comment"}>*/}
-                            {/*            <p className={"commenter-name"}>mari.aa</p>*/}
-                            {/*            <p className="commenter-comment">Sweety</p>*/}
-                            {/*        </div>*/}
-                            {/*    </div>*/}
-                            {/*</div>*/}
-                            <div className="post-comment-like-count">
-                                <img src={require("./img/postCommentWindow-Icon.png")} alt="" className={"like-count-icon"}/>
-                                <p className="count-number">98 Likes</p>
-                            </div>
-                        </div>
-                            <div className="write-comment">
-                                <input type="text" className={"write-comment-input"} placeholder={"write something..."}/>
-                                <button className="send-comment-btn">send</button>
-                                {/*<div className={"send-comment-btn"}>*/}
-                                {/*    /!*<p className="send-comment-btn-arrow"> send </p>*!/*/}
-                                {/*</div>*/}
+     <div className={"posted-comments"}>
+         <PostedComment/>
+         <PostedComment/>
+         <PostedComment/>
+         {/*<div className={"comment"}>*/}
+         {/*    <img src={require("img/maria-comment.png")} alt="" className={"commenter-img"}/>*/}
+         {/*    <div className={"comment-details"}>*/}
+         {/*        <div className={"commenter-name-and-comment"}>*/}
+         {/*            <p className={"commenter-name"}>mari.aa</p>*/}
+         {/*            <p className="commenter-comment">Sweety</p>*/}
+         {/*        </div>*/}
+         {/*    </div>*/}
+         {/*</div>*/}
 
-                            </div>
+     </div>
+ </div>
+                           <div className="comment-section-bottom">
+                               <div className="post-comment-like-count">
+                                   <img src={require("./img/postCommentWindow-Icon.png")} alt="" className={"like-count-icon"}/>
+                                   <p className="count-number">98 Likes</p>
+                               </div>
+                               <div className="write-comment">
+                                   <input type="text" className={"write-comment-input"} placeholder={"write something..."}/>
+                                   <button className="send-comment-btn">send</button>
+                                   {/*<div className={"send-comment-btn"}>*/}
+                                   {/*    /!*<p className="send-comment-btn-arrow"> send </p>*!/*/}
+                                   {/*</div>*/}
+
+                               </div>
+                           </div>
                     </div>
 
                 </div>
